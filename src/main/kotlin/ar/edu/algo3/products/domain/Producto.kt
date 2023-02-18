f0862261553414d042b4bffd114b8877e222e1b8
Fernando Dodino <fernando.dodino@gmail.com>
2023-02-18 08:28:48 -0300

    2023 version (#1)

--- src/main/kotlin/ar/edu/algo3/products/domain/Producto.kt
+++ src/main/kotlin/ar/edu/algo3/products/domain/Producto.kt
@@ -1,8 +1,15 @@
 package ar.edu.algo3.products.domain
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToMany
 import java.time.LocalDate
-import javax.persistence.*
+
 
 @Entity
 class Producto {
