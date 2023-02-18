23f3cf78997e271b1339b4ef1ca1e81f96ac1660
Fernando Dodino <fernando.dodino@gmail.com>
2023-02-18 08:35:46 -0300

    Added video link, docker-compose explanation & updated code in README

--- src/main/kotlin/ar/edu/algo3/products/service/ProductService.kt
+++ src/main/kotlin/ar/edu/algo3/products/service/ProductService.kt
@@ -2,6 +2,7 @@ package ar.edu.algo3.products.service
 
 import ar.edu.algo3.products.repository.ProductoRepository
 import ar.edu.algo3.products.serializer.ProductoDTO
+import jakarta.transaction.Transactional
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
@@ -15,6 +16,7 @@ class ProductoService {
     @Autowired
     lateinit var productoRepository: ProductoRepository
 
+    @Transactional(Transactional.TxType.NEVER)
     fun buscarProductosRecientes() =
         productoRepository
         .findAll(PageRequest.of(0, PRODUCT_PAGINATION_AMOUNT, Sort.Direction.ASC, "fechaIngreso"))
